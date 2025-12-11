ef001151acb59d58cc3bb60e2954e4647576feaa
tarun0409 <tarun.mohandas@gmail.com>
2025-12-11 13:30:34 +0530

    hello world and variable basics

---
+++ variables_basics/src/main.rs
@@ -0,0 +1,16 @@
+const TOUCHDOWN_POINTS: i32 = 6;
+fn main() {
+    let season: &str = "monsoon";
+    
+    let mut points_scored: i32 = 28;
+    
+    points_scored = 35;
+    
+    let event_time = "06:00";
+
+    let event_time = 6;
+
+    println!("The current season is {season}");
+    println!("I scored {0} points and touch down is {1}", points_scored, TOUCHDOWN_POINTS);
+    println!("Time is {} o'clock", event_time);
+}
